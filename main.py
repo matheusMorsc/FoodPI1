4d6a487cd008c7102d15dfa17b228090559644b9
matheusMorsc <matheus.morsc@gmail.com>
2022-10-26 20:50:24 -0300

    sexto

--- main.py
+++ main.py
@@ -1,70 +1,42 @@
-import json
-from unicodedata import name
-import requests
-import pandas as pd
-import psycopg2
-from fastapi import FastAPI
+import fastapi as _fastapi
+import service as _service
+import starlette.responses as _responses
 
-def create_app():
-    app = FastAPI(__name__)
-    
-    @app.route('/')
-    def index():
-        return 'Teste'
-    return app
-
-app = FastAPI()
-
-def conecta_db():
-    con1 = psycopg2.connect(host='localhost',
-                          database='foodpi',
-                          user='postgres',
-                          password='00019008')
-    return con1
-
-# Função Consulta Tabela Banco
-def consulta_db(sql):
-    con = conecta_db()
-    cur = con.cursor()
-    cur.execute(sql)
-    recset = cur.fetchall()
-    registros = []
-    for rec in recset:
-        registros.append(rec)
-    con.close()
-    return registros
+app = _fastapi.FastAPI()
 
 
 @app.get("/")
-def home():
-    return "É assim que era pra funcionar?"
-
-@app.get("/usuarios")
-def usr():
-    reg = consulta_db('select * from usuarios')
-    df_bd = pd.DataFrame(reg, columns=['uId','uNome', 'uTelefone','uEndereco', 'uEmail', 'uUsername', 'uSenha'])
-    print(df_bd)
-
-    return reg
-
-@app.get("/user1/{uId_usr}")
-def usr(uId_usr:str):
-    sql = "select * from usuarios where id = "+uId_usr
-    reg = consulta_db(sql)
-    df_bd = pd.DataFrame(reg, columns=['uId','uNome', 'uTelefone','uEndereco', 'uEmail', 'uUsername', 'uSenha'])
-    print(df_bd)
-
-    return reg
+async def root():
+    return _responses.RedirectResponse("/redoc")
 
 
-#@app.get("/user2/{codigo_usr},{nome}")
-#def usr(codigo_usr:str,nome : str):
- #   sql = "select * from cliente where codigo = "+codigo_usr+" AND nome = '"+nome+"'"
-  #  reg = consulta_db(sql)
-   # df_bd = pd.DataFrame(reg, columns=['codigo','nome','endereco', 'telefone'])
-    #print(df_bd)
+@app.get("/events/today")
+async def today():
+    return _service.todays_events()
 
-    #return reg
 
-reg = consulta_db('select * from usuarios')
-print(reg)
+@app.get("/events")
+async def events():
+    return _service.get_all_events()
+
+
+@app.get("/events/{month}")
+async def events_month(month: str):
+    month_events = _service.month_events(month)
+    if month_events:
+        return month_events
+
+    return _fastapi.HTTPException(
+        status_code=404, detail=f"Month: {month} could not be found"
+    )
+
+
+@app.get("/events/{month}/{day}")
+async def events_of_day(month: str, day: int):
+    events = _service.day_events(month, day)
+    if events:
+        return events
+
+    return _fastapi.HTTPException(
+        status_code=404, detail=f"Date: {month}/{day} could not be found"
+    )
