fe44fc23f627f9edb0527941602eadadf34cd835
matheusMorsc <matheus.morsc@gmail.com>
2022-11-09 15:37:21 -0300

    teste

--- main.py
+++ main.py
@@ -58,6 +58,17 @@ async def shutdown():
     await database.disconnect()
   
 
+@app.get("/menu/", response_model=List[Menu])   
+async def read_restaurantes():
+    query = menus.select()
+    return await database.fetch_all(query) 
+
+@app.post("/menu/", response_model=Menu)   
+async def create_restaurantes(menu: MenuIn):
+    query = menus.insert().values(nome=menu.nome, img=menu.img, preco=menu.preco, revisao=menu.revisao, avaliacao=menu.avaliacao)
+    last_record_id = await database.execute(query)
+    return {**menu.dict(), "id": last_record_id}
+
 @app.put("/menu/{menu_id}", response_model=Menu)
 async def create_restaurantes(menu: Menu):
     query = menus.update().values(nome=menu.nome, img=menu.img, preco=menu.preco, revisao=menu.revisao, avaliacao=menu.avaliacao)
