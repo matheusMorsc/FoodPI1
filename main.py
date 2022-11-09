404c46d739f5059dd0defcd6748ed3a8ef658263
matheusMorsc <matheus.morsc@gmail.com>
2022-11-09 15:58:42 -0300

    teste

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI
 import databases
 import sqlalchemy
 from pydantic import BaseModel
-import models
  
 DATABASE_URL = "postgresql://ftrhsjtstmpeje:17df4f3741976fb20e3905810153598ba865061e72b65f7954a94832f1231c92@ec2-18-215-41-121.compute-1.amazonaws.com:5432/d3am4gch1nbf8m"
 
@@ -18,10 +17,20 @@ menus = sqlalchemy.Table(
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("nome", sqlalchemy.String),
     sqlalchemy.Column("img", sqlalchemy.String),
-    sqlalchemy.Column("preco", sqlalchemy.Integer),
+    sqlalchemy.Column("preco", sqlalchemy.String),
     sqlalchemy.Column("revisao", sqlalchemy.Integer),
     sqlalchemy.Column("avaliacao", sqlalchemy.Integer),
+    sqlalchemy.Column("categoria", sqlalchemy.String),
+)
 
+itens = sqlalchemy.Table(
+    "itens",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("title", sqlalchemy.String),
+    sqlalchemy.Column("description", sqlalchemy.String),
+    sqlalchemy.Column("price", sqlalchemy.String),
+    sqlalchemy.Column("image", sqlalchemy.String),
 )
 
 engine = sqlalchemy.create_engine(
@@ -34,18 +43,31 @@ class Menu(BaseModel):
     id: int
     nome: str
     img: str
-    preco: int
+    preco: str
     revisao: int
     avaliacao: int
-    
-
+    categoria: str
 
 class MenuIn(BaseModel):
     nome: str
     img: str
-    preco: int
+    preco: str
     revisao: int
     avaliacao: int
+    categoria: str
+
+class Item(BaseModel):
+    id: int
+    title: str
+    description: str
+    price: str
+    image: str
+
+class ItemIn(BaseModel):
+    title: str
+    description: str
+    price: str
+    image: str
 
 app = FastAPI()
 
@@ -56,4 +78,36 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+  
+@app.get("/menu/", response_model=List[Menu])   
+async def read_restaurantes():
+    query = menus.select()
+    return await database.fetch_all(query) 
 
+@app.post("/menu/", response_model=Menu)   
+async def create_restaurantes(menu: MenuIn):
+    query = menus.insert().values(nome=menu.nome, img=menu.img, preco=menu.preco, revisao=menu.revisao, avaliacao=menu.avaliacao, categoria=menu.categoria)
+    last_record_id = await database.execute(query)
+    return {**menu.dict(), "id": last_record_id}
+
+@app.get("/item/", response_model=List[Item])   
+async def read_item():
+    query = itens.select()
+    return await database.fetch_all(query)
+
+@app.get("/item/{id}", response_model=List[Item])   
+async def read_item_by_id(id:int):
+    query = itens.select().where(itens.c.id == id)
+    return await database.fetch_all(query)
+
+@app.post("/item/", response_model=Item)   
+async def create_item(item: ItemIn):
+    query = itens.insert().values(title=item.title, image=item.image, price=item.price, description=item.description)
+    last_record_id2 = await database.execute(query)
+    return {**item.dict(), "id": last_record_id2}
+
+@app.put("/item/", response_model=Item)   
+async def update_item(item: ItemIn):
+    query = itens.insert().values(title=item.title, image=item.image, price=item.price, description=item.description)
+    last_record_id2 = await database.execute(query)
+    return {**item.dict(), "id": last_record_id2}
