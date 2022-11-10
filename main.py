527f7ef972e96da7572b41e9d03151f4d0b3fefa
matheusMorsc <matheus.morsc@gmail.com>
2022-11-10 14:09:13 -0300

    teste

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from urllib import response
 from typing import List
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 import databases
 import sqlalchemy
 from pydantic import BaseModel
- 
+from fastapi.security import OAuth2PasswordBearer
+
+
 DATABASE_URL = "postgresql://ftrhsjtstmpeje:17df4f3741976fb20e3905810153598ba865061e72b65f7954a94832f1231c92@ec2-18-215-41-121.compute-1.amazonaws.com:5432/d3am4gch1nbf8m"
 
 database = databases.Database(DATABASE_URL)
@@ -27,6 +29,7 @@ itens = sqlalchemy.Table(
     "itens",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("subid", sqlalchemy.Integer),
     sqlalchemy.Column("title", sqlalchemy.String),
     sqlalchemy.Column("description", sqlalchemy.String),
     sqlalchemy.Column("price", sqlalchemy.String),
@@ -58,18 +61,22 @@ class MenuIn(BaseModel):
 
 class Item(BaseModel):
     id: int
+    subid: int
     title: str
     description: str
     price: str
     image: str
 
 class ItemIn(BaseModel):
+    subid: int
     title: str
     description: str
     price: str
     image: str
 
 app = FastAPI()
+
+## INCICIO E FIM
 
 @app.on_event("startup")
 async def startup():
@@ -78,6 +85,8 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+    
+## RESTAURANTES
   
 @app.get("/menu/", response_model=List[Menu])   
 async def read_restaurantes():
@@ -90,19 +99,33 @@ async def create_restaurantes(menu: MenuIn):
     last_record_id = await database.execute(query)
     return {**menu.dict(), "id": last_record_id}
 
+## ITENS
+
 @app.get("/item/", response_model=List[Item])   
 async def read_item():
     query = itens.select()
     return await database.fetch_all(query)
 
-@app.get("/item/{id}", response_model=List[Item])   
-async def read_item_by_id(id:int):
-    query = itens.select().where(itens.c.id == id)
+@app.get("/item/{subid}", response_model=List[Item])   
+async def read_item_by_id(subid:int):
+    query = itens.select().where(itens.c.subid == subid)
     return await database.fetch_all(query)
-
+    
 @app.post("/item/", response_model=Item)   
 async def create_item(item: ItemIn):
-    query = itens.insert().values(title=item.title, image=item.image, price=item.price, description=item.description)
+    query = itens.insert().values(title=item.title, image=item.image, price=item.price, description=item.description, subid=item.subid)
     last_record_id2 = await database.execute(query)
     return {**item.dict(), "id": last_record_id2}
+
+
+## Cadastro e login 
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
+
+
 
