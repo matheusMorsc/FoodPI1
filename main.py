72fca55c4e8172fc39e4ba8cf4b9f15ce1aa0e5b
matheusMorsc <matheus.morsc@gmail.com>
2022-11-11 10:06:45 -0300

    teste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from urllib import response
 from typing import List
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 import databases
 import sqlalchemy
 from pydantic import BaseModel
@@ -127,13 +127,17 @@ def fake_decode_token(token):
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     user = fake_decode_token(token)
     if not user:
-       print("Credenciais inválidas")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return user
 
 
 async def get_current_active_user(current_user: User = Depends(get_current_user)):
     if current_user.disabled:
-       print("Inactive user")
+        raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 
@@ -141,11 +145,11 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user_dict = fake_users_db.get(form_data.username)
     if not user_dict:
-       print("Incorrect username or password")
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
     user = UserInDB(**user_dict)
     hashed_password = fake_hash_password(form_data.password)
     if not hashed_password == user.hashed_password:
-        print("Incorrect username or password")
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
 
     return {"access_token": user.username, "token_type": "bearer"}
 
