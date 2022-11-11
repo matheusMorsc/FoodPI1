ce9f10c509bab03ae1aa4856d0c0cb6793776196
matheusMorsc <matheus.morsc@gmail.com>
2022-11-11 09:57:20 -0300

    teste

--- main.py
+++ main.py
@@ -77,13 +77,29 @@ app = FastAPI()
 ## LOGIN 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
 
-@app.get("/items/")
-async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
 
 
-## INCICIO E FIM
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
+## INICIO E FIM
 
 @app.on_event("startup")
 async def startup():
